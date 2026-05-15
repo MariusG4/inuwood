0d67448e3556b854c8a676f664baf5ce317b3032
Setzeph <garai.marius@gmail.com>
2026-05-15 17:03:45 +0300

    astro 6 fixess

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://inuwood.ro',
-  integrations: [tailwind()],
   output: 'static',
   compressHTML: true,
 });
